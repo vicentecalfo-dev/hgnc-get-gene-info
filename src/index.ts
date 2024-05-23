350a4fa852c07db4bc1991d20ced63f4c6ab431f
Vicente Calfo <vicentecalfo@gmail.com>
2024-05-23 14:31:53 -0300

    feat: implementação dos métodos de busca

---
+++ src/index.ts
@@ -0,0 +1,178 @@
+interface HGNCOptions {
+  rootURL?: string;
+  output?: "XML" | "JSON";
+}
+
+class HGNC {
+  private rootURL: string;
+  private output: "XML" | "JSON";
+  private contentType = {
+    XML: "text/xml",
+    JSON: "application/json",
+  };
+  constructor({
+    rootURL = "https://rest.genenames.org",
+    output = "JSON",
+  }: HGNCOptions = {}) {
+    this.rootURL = rootURL;
+    this.output = output;
+  }
+
+  private responseByContentType(response: Response) {
+    return this.output === "JSON" ? response.json() : response.text();
+  }
+
+  private customFetch(resource: string) {
+    return fetch(`${this.rootURL}/${resource}`, {
+      method: "GET",
+      headers: {
+        Accept: this.contentType[this.output],
+      },
+    });
+  }
+
+  get storedFields() {
+    return [
+      "cd",
+      "iuphar",
+      "pseudogene.org",
+      "status",
+      "lncrnadb",
+      "horde_id",
+      "mirbase",
+      "prev_name",
+      "prev_symbol",
+      "gene_group",
+      "uniprot_ids",
+      "date_modified",
+      "location",
+      "gtrnadb",
+      "merops",
+      "symbol_report_tag",
+      "ena",
+      "rgd_id",
+      "alias_name",
+      "refseq_accession",
+      "homeodb",
+      "mane_select",
+      "location",
+      "bioparadigms_slc",
+      "orphanet",
+      "snornabase",
+      "cosmic",
+      "rna_central_id",
+      "alias_symbol",
+      "imgt",
+      "gene_group_id",
+      "vega_id",
+      "ucsc_id",
+      "lsdb",
+      "ensembl_gene_id",
+      "date_symbol_changed",
+      "date_name_changed",
+      "date_approved_reserved",
+      "locus_type",
+      "symbol",
+      "pubmed_id",
+      "alias_name",
+      "omim_id",
+      "enzyme_id",
+      "_version_",
+      "name",
+      "mamit-trnadb",
+      "lncipedia",
+      "mgd_id",
+      "curator_notes",
+      "gencc",
+      "ccds_id",
+      "agr",
+      "uuid",
+      "hgnc_id",
+      "prev_name",
+      "entrez_id",
+      "locus_group",
+    ];
+  }
+
+  get searchableFields() {
+    return [
+      "symbol",
+      "rna_central_id",
+      "alias_name",
+      "curator_notes",
+      "rgd_id",
+      "locus_type",
+      "alias_symbol",
+      "ucsc_id",
+      "ensembl_gene_id",
+      "ccds_id",
+      "locus_group",
+      "ena",
+      "mgd_id",
+      "name",
+      "refseq_accession",
+      "hgnc_id",
+      "entrez_id",
+      "prev_symbol",
+      "omim_id",
+      "symbol_report_tag",
+      "status",
+      "vega_id",
+      "mane_select",
+      "uniprot_ids",
+      "prev_name",
+      "location",
+    ];
+  }
+
+  async fetchInfo() {
+    try {
+      const response = await this.customFetch("info");
+      return await this.responseByContentType(response);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async fetchBy(storedField: string, query: any) {
+    const fieldNotExists = !this.storedFields.includes(storedField);
+    if (fieldNotExists)
+      throw new Error(
+        "The provided resource is not valid. Please verify the options for storedFields."
+      );
+    try {
+      const resource = `fetch/${storedField}/${encodeURIComponent(query)}`;
+      const response = await this.customFetch(resource);
+      return await this.responseByContentType(response);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async searchByAllFields(query: any) {
+    try {
+      const resource = `search/${encodeURIComponent(query)}`;
+      const response = await this.customFetch(resource);
+      return await this.responseByContentType(response);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async searchBy(searchableField: string, query: any) {
+    const fieldNotExists = !this.searchableFields.includes(searchableField);
+    if (fieldNotExists)
+      throw new Error(
+        "The provided resource is not valid. Please verify the options for searchableFields."
+      );
+    try {
+      const resource = `search/${searchableField}/${encodeURIComponent(query)}`;
+      const response = await this.customFetch(resource);
+      return await this.responseByContentType(response);
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export { HGNC };
---
+++ tests/index.test.ts
@@ -0,0 +1,61 @@
+import { HGNC } from "../dist";
+
+const hgnc = new HGNC();
+
+const searchableFieldsData = hgnc.searchableFields;
+const storedFieldsData = hgnc.storedFields;
+
+function arraysAreEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    const sortedArr1 = [...arr1].sort();
+    const sortedArr2 = [...arr2].sort();
+    return sortedArr1.every((value, index) => value === sortedArr2[index]);
+}
+
+test("Get INFO - [searchableFields]", async () => {
+    const { searchableFields } = await hgnc.fetchInfo();
+    const test = arraysAreEqual(searchableFieldsData, searchableFields);
+    expect(test).toBe(true);
+});
+
+test("Get INFO - [storedFields]", async () => {
+    const { storedFields } = await hgnc.fetchInfo();
+    const test = arraysAreEqual(storedFieldsData, storedFields);
+    expect(test).toBe(true);
+});
+
+test("Fetch by symbol = ZNF3", async () => {
+    const resource = "symbol";
+    const query = "ZNF3";
+    const { response } = await hgnc.fetchBy(resource, query);
+    expect(response.docs[0][resource]).toBe(query);
+});
+
+test("Fetch by locus_type ='gene with protein product'", async () => {
+    const resource = "locus_type";
+    const query = "gene with protein product";
+    const data = await hgnc.fetchBy(resource, query);
+    expect(data.response.docs[0][resource]).toBe(query);
+});
+
+test("Fetch by ensembl_gene_id = ENSG00000166526", async () => {
+    const resource = "ensembl_gene_id";
+    const query = "ENSG00000166526";
+    const { response } = await hgnc.fetchBy(resource, query);
+    expect(response.docs[0][resource]).toBe(query);
+});
+
+test("Searching by all fields = BRAF", async () => {
+    const query = "BRAF";
+    const { response } = await hgnc.searchByAllFields(query);
+    expect(response.docs.length).toBe(5);
+});
+
+test("Searching by symbol = ZNF*", async () => {
+    const resource = "symbol";
+    const query = "ZNF*";
+    const { response } = await hgnc.searchBy(resource, query);
+    expect(response.docs.length).toBe(764);
+});
